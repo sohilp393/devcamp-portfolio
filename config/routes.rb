722c3a2ede02ae301a71e95cb9d2e6164eceb98f
sohilp393 <Sohilp393@gmail.com>
2020-04-10 17:51:25 +0530

    Data Flow in rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
+  resources :portfolios,except: [:show]
+
+  get 'portfolio/:id',to: 'portfolios#show',as: 'portfolio_show' 
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
   get 'pages/contact'
-  resources :blogs
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
+
+  root to: 'pages#home'
 end
