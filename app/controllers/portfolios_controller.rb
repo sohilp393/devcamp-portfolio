dd30f58f9dec924fdef014f124a08d670ef95448
sohilp393 <Sohilp393@gmail.com>
2020-04-14 13:09:45 +0530

    Refactored application controller to include concern for source feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -15,7 +15,7 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
-    3.times {@portfolio_item.technologies.build}
+    4.times {@portfolio_item.technologies.build}
   end
 
   def edit
