b36002eee6d21f847370a4e39cda4e7ef8beb721
sohilp393 <Sohilp393@gmail.com>
2020-04-15 13:39:35 +0530

    Added Petergate gem

--- Gemfile
+++ Gemfile
@@ -50,4 +50,4 @@ gem 'devise','~>4.2'
 
 gem 'bootstrap', '~> 4.4.1'
 gem 'jquery-rails'
-
+gem 'petergate', '~> 2.0', '>= 2.0.1'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,21 @@ class ApplicationController < ActionController::Base
   include DeviseWhitelist
   include SetSource
   include CurrentUserConcern
-  include DefaultPageContent
+  include DefaultPageContent                                 
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = DevcampViewTool::Renderer.copyright 'Sohil' ,'All rights reserved'
+  end
+
+
+  module DevcampViewTool
+    class Renderer
+      def self.copyright name,msg
+        "&copy: #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+      end
+    end
+  end
+
 end
