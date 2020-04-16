a66b5845881c840e0e4c4b21ff70f5b2f5bacb39
sohilp393 <Sohilp393@gmail.com>
2020-04-16 16:07:15 +0530

    Installed jquery ui,sortable html library,css calls

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,3 +19,7 @@ require("channels")
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require jquery_ujs
+//= require jquery_ui
+//= require html,sortable
+//= require_tree 
