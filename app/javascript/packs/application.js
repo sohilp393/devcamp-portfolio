5953489d7f1f009c464e0133122b0ab13f841d64
sohilp393 <Sohilp393@gmail.com>
2020-04-16 18:06:44 +0530

    Implemented authorization rules for portfolio controller and view for drag and drop feature

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+require("packs/html.sortable")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -21,5 +21,4 @@ require("channels")
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require jquery_ui
-//= require html,sortable
 //= require_tree 
