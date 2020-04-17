e488b9e0f559e75382f754b6c478cfaa1b816abe
sohilp393 <Sohilp393@gmail.com>
2020-04-17 20:38:00 +0530

    Refactored alerts into helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -52,4 +52,17 @@ module ApplicationHelper
   def active? path
     "active" if current_page? path
   end
+
+  def alerts
+    alert = (flash[:alert] || flash[:error]|| flash[:notice])
+
+    if alert
+      alert_generator alert      
+    end
+
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title:"Please pay attemtion!",sticky: false)
+  end
 end
