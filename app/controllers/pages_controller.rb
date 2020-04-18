d05c2fac4501e98fc80012e359904c23226efc49
sohilp393 <Sohilp393@gmail.com>
2020-04-18 13:13:59 +0530

    Built twitter parser for tweet links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,9 @@ class PagesController < ApplicationController
   end
 
   def tech_news
-    @tweets = SocialTool.twitter_search 
+    @tweets = []
+    @tweets <<  "Kudos to all those working round the clock, across the nation, to ensure India’s energy needs are met."
+    @tweets  << "Go Fun The World. Get 9GAG app for the latest memes, gifs and videos: http://9gag.com/apps. Contact us: 9gag@9gag.com"
+    #@tweets = SocialTool.twitter_search 
   end
 end
