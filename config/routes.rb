bf6b3f0eeb4e9e1dfa5eef21880f8def84f6ce5c
sohilp393 <Sohilp393@gmail.com>
2020-04-18 12:47:49 +0530

    Implemented twitter call on tech new page

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,7 @@ Rails.application.routes.draw do
   get 'portfolio/:id',to: 'portfolios#show',as: 'portfolio_show' 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-
-  get 'pages/contact'
+  get 'tech-news' , to:'pages#tech_news'
  
   resources :blogs do
     member do
