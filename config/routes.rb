9df3b02d4f6c9530f8f59ea58c8d73a56ef72f2f
sohilp393 <Sohilp393@gmail.com>
2020-04-23 20:54:33 +0530

    Added route for archives page

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,14 @@ Rails.application.routes.draw do
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
   end
+
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   get 'tech-news', to: 'pages#tech_news'
-
+  get 'archives', to: 'blogs#archives'
 
   resources :blogs do
     member do
@@ -18,6 +19,5 @@ Rails.application.routes.draw do
   end
 
   mount ActionCable.server => '/cable'
-
   root to: 'pages#home'
 end
