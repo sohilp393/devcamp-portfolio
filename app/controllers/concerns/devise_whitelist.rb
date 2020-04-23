b0a0a64adf3dbad814416fe3b6df21d2d0df0352
sohilp393 <Sohilp393@gmail.com>
2020-04-23 20:45:04 +0530

    ..

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -5,7 +5,7 @@ module DeviseWhitelist
     before_action :configure_permitted_parameters,if: :devise_controller?
   end
     
-  def configure_permitted_parameters
+  def configure_permitted_parameters 
     devise_parameter_sanitizer.permit(:sign_up,keys: [:name])
     devise_parameter_sanitizer.permit(:account_update,keys: [:name])
   end 
