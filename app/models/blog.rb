f4cd0c3936fe345b4a0b8cf73e7df2453f030431
sohilp393 <Sohilp393@gmail.com>
2020-04-28 12:19:57 +0530

    modified the methods

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,5 @@
 class Blog < ApplicationRecord
-  enum status: { draft: 0 , published: 1}
+  enum status: {draft: 0 , published: 1}
   extend FriendlyId
   friendly_id :title , use: :slugged
 
@@ -13,19 +13,13 @@ class Blog < ApplicationRecord
     blogsbetweendates(archivedate)
   end
 
-  def self.bloglastmonth
-    date = Date.today.prev_month
-    blogsbetweendates(date)  
-  end
-
-  def self.blogpenultimate
-    date = Date.today.prev_month.prev_month
-    blogsbetweendates(date)
+  def self.blogscount date
+    blogsbetweendates(date).count  
   end
 
   def self.blogsbetweendates date
     start_date = date.to_date.at_beginning_of_month
     end_date = date.to_date.at_end_of_month
-    Blog.where(:created_at => start_date..end_date)
+    where(:created_at => start_date..end_date)
   end
 end
